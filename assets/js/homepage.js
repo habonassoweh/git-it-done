90df6e79927a2dc7380c34a4dd1bf7caf3cbd696
Habon Assoweh <habonassoweh@Habons-Air>
2022-04-13 16:27:41 -0400

    see list of all open issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,24 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var formSubmitHandler = function (event) {
+  //prevent page from refreshing
+  event.preventDefault();
+
+  //get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a Github username");
+  }
+};
+
 var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -24,35 +42,17 @@ var getUserRepos = function (user) {
       //Notice this `.catch()` getting chained onto the end of the .`then()` method
       alert("Unable to connect to GitHub");
     });
+};
 
+//create a function to display repos
+var displayRepos = function (repos, searchTerm) {
   //check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
     return;
   }
-};
-
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  //get value from input element
-  var username = nameInputEl.value.trim();
-
-  if (username) {
-    getUserRepos(username);
-    nameInputEl.value = "";
-  } else {
-    alert("Please enter a Github username");
-  }
-};
-
-//create a function to disply repos
-var displayRepos = function (repos, searchTerm) {
-  console.log(repos);
-  console.log(searchTerm);
 
   //clear old content
-  repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
 
   //loop over repos
@@ -60,9 +60,10 @@ var displayRepos = function (repos, searchTerm) {
     //format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    //create a container for each repo
-    var repoEl = document.createElement("div");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     //create a span element to hold repository name
     var titleEl = document.createElement("span");
@@ -94,4 +95,5 @@ var displayRepos = function (repos, searchTerm) {
   }
 };
 
+//add event listener to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,19 +1,43 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var getRepoName = function () {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function (repo) {
-  console.log(repo);
-
+  //format the github api URL
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+  //make a get request to URL
   fetch(apiUrl).then(function (response) {
     //request was successful
     if (response.ok) {
       response.json().then(function (data) {
         //pass response data to DOM function
         displayIssues(data);
+
+        ///check if api had paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
-      alert("There was a problem with your request!");
+      //if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
@@ -53,4 +77,18 @@ var displayIssues = function (issues) {
   issueContainerEl.appendChild(issueEl);
 };
 
-getRepoIssues("habonassoweh/deep-thoughts");
+var displayWarning = function (repo) {
+  //add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See more issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "blank");
+
+  //append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
+getRepoIssues();
